e4f8724193ab339703bfc7d9ae006e9b8d406148
Chaimae Bouassab <140265140+ChaimaeBouassab@users.noreply.github.com>
2025-01-02 15:30:20 +0100

    Added files

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  @ViewChild('container')
+  container!: ElementRef;
+
+  signIn() {
+    this.container.nativeElement.classList.remove('right-panel-active');
+  }
+
+  signUp() {
+    this.container.nativeElement.classList.add('right-panel-active');
+  }
+}
